b2fc29a89fc2d56558a428d62a67377e85c9fd34
Eddie Melo <heriberto@melo.nyc>
2017-12-15 15:16:30 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 new_hash = programmer_hash[:dennis_ritchie][:languages][0]
+		 new_hash = programmer_hash[:dennis_ritchie][:languages]
 		 puts "#{new_hash}"
 	return programmer_hash
 end
