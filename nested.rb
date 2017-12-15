7a6789eb035e4a038095c6b383f5c6dd96768526
Eddie Melo <heriberto@melo.nyc>
2017-12-15 15:16:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 new_hash = programmer_hash[:dennis_ritchie][:languages]
+		 new_hash = programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
 		 puts "#{new_hash}"
 	return programmer_hash
 end
